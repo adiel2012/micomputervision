2d786a7d4e53bba644be48f24ee954d5e9748637
LABORATORIO #3 <LABORATORIO #3@LAB3PCPROFESOR>
2016-08-27 10:33:47 -0500

    segundo

---
+++ src/geometry/Transformer2D.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package geometry;
+
+import core.Point2DDouble;
+import core.Point2DInteger;
+
+/**
+ *
+ * @author LABORATORIO #3
+ */
+//  http://szeliski.org/Book/drafts/SzeliskiBook_20100903_draft.pdf
+public class Transformer2D {
+    public Point2DDouble traslate(double incx, double incy){
+       return null;
+    }
+    
+    public Point2DInteger traslate(int incx, int incy){
+       return null;
+    }
+    
+}
